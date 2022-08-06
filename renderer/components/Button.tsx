fb9d483f7f4fb75cf29b2f847a322c63058a55e5
LeeSeungHun <dos07008@naver.com>
2022-08-07 02:53:39 +0900

    feat:Button

---
+++ renderer/components/Button.tsx
@@ -0,0 +1,16 @@
+import { theme } from "../style/theme";
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: ${theme.spacing[8]} ${theme.spacing[16]};
+  background-color: ${theme.colors.skyblue};
+  border-radius: ${theme.spacing[8]};
+  outline: none;
+  border: none;
+  :active {
+    background-color: ${theme.colors.turquoise2};
+  }
+  :disabled {
+    background-color: ${theme.colors.gray};
+  }
+`;
