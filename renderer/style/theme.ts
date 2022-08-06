45bfce7ff80593b338fd392d12dd4749692c541d
hautest <100465780+hautest@users.noreply.github.com>
2022-08-07 02:11:17 +0900

    Feat/input (#4)
    * Typography 폴더 삭제,  red 컬러 추가, spacing 추가
    
    * feat:input

--- renderer/style/theme.ts
+++ renderer/style/theme.ts
@@ -5,12 +5,17 @@ const colors = {
   turquoise1: "#27496D",
   turquoise2: "#0C7B93",
   skyblue: "#00A8CC",
+  red: "#FE5432",
 };
 
 const spacing = {
   "4": "4px",
   "8": "8px",
+  "12": "12px",
   "16": "16px",
+  "20": "20px",
+  "24": "24px",
+  "28": "28px",
   "32": "32px",
   "62": "64px",
 };
