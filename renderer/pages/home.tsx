fd073c5f97e5ef2357d3d4ca8b0e33ae62c55aed
LeeSeungHun <dos07008@naver.com>
2022-08-06 22:33:56 +0900

    초기 세팅, styled-components 설치

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home - Nextron (with-typescript-material-ui)</title>
+      </Head>
+      <div>12</div>
+    </>
+  );
+}
+
+export default Home;
