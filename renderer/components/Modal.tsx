86ed2377cb5d4e1baa56145f32b3890e04e9c433
LeeSeungHun <dos07008@naver.com>
2022-08-07 06:26:45 +0900

    feat:modal

---
+++ renderer/components/Modal.tsx
@@ -0,0 +1,50 @@
+import { ReactNode, MouseEvent, FC, useState, useEffect } from "react";
+import ReactDOM, { createPortal } from "react-dom";
+import { theme } from "../style";
+import styled from "styled-components";
+
+interface ModalInterface {
+  children: ReactNode;
+  visible: boolean;
+  onClick?: (event?: MouseEvent<HTMLDivElement>) => void;
+}
+
+function Portal({ children }: Pick<ModalInterface, "children">) {
+  const [element, setElement] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setElement(document.getElementById("modal"));
+  }, []);
+
+  if (!element) {
+    return <></>;
+  }
+
+  return ReactDOM.createPortal(children, element);
+}
+
+export function Modal({ children, visible, onClick }: ModalInterface) {
+  return (
+    <Portal>
+      <StyledModal visible={visible} onClick={onClick}>
+        {children}
+      </StyledModal>
+    </Portal>
+  );
+}
+
+const StyledModal: FC<ModalInterface> = styled.div<ModalInterface>`
+  visibility: ${({ visible }) => (visible ? "visible" : "hidden")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.black};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  height: 100%;
+  opacity: 0.3;
+`;
--- renderer/firebase.ts
+++ renderer/firebase.ts
@@ -1,13 +1,7 @@
-// Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-
 import "firebase/compat/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -23,5 +17,5 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
-// setPersistence(authService, browserLocalPersistence);
+
 export const dbService = firebase.firestore();
