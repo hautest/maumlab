dd09aac86dc88877a785a90a8efc17e1cd7ee10b
LeeSeungHun <dos07008@naver.com>
2022-08-07 21:50:36 +0900

    input 수정

--- renderer/components/Input.tsx
+++ renderer/components/Input.tsx
@@ -40,7 +40,6 @@ const StyledInput = styled.input<{ state: boolean }>`
   width: 100%;
   outline: none;
   padding: ${theme.spacing[12]} ${theme.spacing[16]};
-  height: 100%;
   border-radius: ${theme.spacing[8]};
   border: solid 1px
     ${({ state }) => (state ? theme.colors.skyblue : theme.colors.red)};
@@ -51,7 +50,6 @@ const StyledInput = styled.input<{ state: boolean }>`
 
 const StyledTextarea = styled.textarea<{ state: boolean }>`
   width: 100%;
-  height: 100%;
   outline: none;
   padding: ${theme.spacing[12]} ${theme.spacing[16]};
   border-radius: ${theme.spacing[8]};
