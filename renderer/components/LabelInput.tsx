d4f7f2c8e5105c70a8028bfb65abb4522d16e4de
LeeSeungHun <dos07008@naver.com>
2022-08-07 16:20:17 +0900

    feat:LabelInput

---
+++ renderer/components/LabelInput.tsx
@@ -0,0 +1,32 @@
+import { Typography } from "./Typography";
+import { Input, InputProps } from "./Input";
+
+type LabelInputProps = Pick<
+  InputProps,
+  "value" | "onChange" | "id" | "children" | "state" | "type"
+>;
+
+export function LabelInput({
+  value,
+  onChange,
+  id,
+  children,
+  type,
+  state,
+}: LabelInputProps) {
+  return (
+    <>
+      <label>
+        <Typography type="subtitle">{children}</Typography>
+      </label>
+      <Input
+        id={id}
+        type={type}
+        state={state}
+        element="input"
+        value={value}
+        onChange={onChange}
+      />
+    </>
+  );
+}
