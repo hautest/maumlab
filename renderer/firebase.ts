c21f246b78a633d0289ca2476cff8c82db17b24c
LeeSeungHun <dos07008@naver.com>
2022-08-10 04:46:15 +0900

    firebase key값 환경 변수

--- renderer/firebase.ts
+++ renderer/firebase.ts
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth, browserLocalPersistence } from "firebase/auth";
+import { initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBu2rDVRiyhsugpLWAsrDWrhoVZmiM4WEo",
-  authDomain: "test-firebase-d594b.firebaseapp.com",
-  projectId: "test-firebase-d594b",
-  storageBucket: "test-firebase-d594b.appspot.com",
-  messagingSenderId: "379321634435",
-  appId: "1:379321634435:web:9c344fc026c961c06095b6",
-  measurementId: "G-LEG7MYXHN8",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 export const fb = initializeApp(firebaseConfig, "maumlab");
